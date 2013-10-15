cbc80952e053d594aa0dc884a263f55834c09be7
tfreese <commercial@freese-home.de>
2013-10-15 18:25:13 +0200

    layoutmanager geändert

--- de.freese.jripper/jripper-swing/src/main/java/de/freese/jripper/swing/JRipperSwing.java
+++ de.freese.jripper/jripper-swing/src/main/java/de/freese/jripper/swing/JRipperSwing.java
@@ -7,11 +7,15 @@ package de.freese.jripper.swing;
 import de.freese.jripper.swing.action.ActionCDDBQuery;
 import de.freese.jripper.swing.action.ActionRipping;
 import de.freese.jripper.swing.table.AlbumTableModel;
-import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -25,6 +29,37 @@ import javax.swing.WindowConstants;
  */
 public class JRipperSwing
 {
+	/**
+	 * Liefert default GridBagConstraints mit fill=BOTH, anchor=CENTER und Insets mit (5, 5, 5, 5).
+	 * 
+	 * @param col int, siehe auch Konstanten in GridBagConstraints
+	 * @param row int, siehe auch Konstanten in GridBagConstraints
+	 * @return {@link GridBagConstraints}
+	 */
+	public static GridBagConstraints getGBC(final int col, final int row)
+	{
+		GridBagConstraints constraints = new GridBagConstraints();
+
+		constraints.gridx = col;
+		constraints.gridy = row;
+		constraints.gridwidth = 1;
+		constraints.gridheight = 1;
+		constraints.weightx = 1.0;
+		constraints.weighty = 1.0;
+		constraints.ipadx = 0;
+		constraints.ipady = 0;
+		constraints.fill = GridBagConstraints.BOTH;
+		constraints.anchor = GridBagConstraints.CENTER;
+		constraints.insets = new Insets(5, 5, 5, 5);
+
+		return constraints;
+	}
+
+	/**
+	 * @param container {@link Container}
+	 * @param table {@link JTable}
+	 */
+
 	/**
 	 * @param args String[]
 	 */
@@ -63,13 +98,17 @@ public class JRipperSwing
 		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 
-		frame.setLayout(new BorderLayout());
+		frame.setLayout(new GridBagLayout());
 
 		JTable table = new JTable();
 		table.setModel(new AlbumTableModel());
 
 		initMenue(frame, table);
-		frame.add(new JScrollPane(table), BorderLayout.CENTER);
+		initSettings(frame);
+
+		GridBagConstraints gbc = getGBC(0, 1);
+		gbc.gridwidth = 4;
+		frame.add(new JScrollPane(table), gbc);
 
 		frame.setVisible(true);
 	}
@@ -85,6 +124,26 @@ public class JRipperSwing
 		panel.add(new JButton(new ActionCDDBQuery(table)));
 		panel.add(new JButton(new ActionRipping(table)));
 
-		container.add(panel, BorderLayout.NORTH);
+		GridBagConstraints gbc = getGBC(0, 0);
+		gbc.gridwidth = 6;
+		gbc.weighty = 0.0D;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.anchor = GridBagConstraints.NORTH;
+		container.add(panel, gbc);
+	}
+
+	/**
+	 * @param container {@link Container}
+	 */
+	private void initSettings(final Container container)
+	{
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		panel.setBorder(BorderFactory.createTitledBorder("Settings"));
+
+		panel.add(new JLabel("Work Dir.:"));
+
+		GridBagConstraints gbc = getGBC(5, 1);
+		container.add(panel, gbc);
 	}
 }
